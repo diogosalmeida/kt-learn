7ed28987317a8dd94399386400cf4e9d0240eb19
diogosalmeida <diogodsalmeida@gmail.com>
2021-09-01 02:13:00 -0300

    Create passagener crud

--- src/main/kotlin/com/kt/learn/domain/Entities.kt
+++ src/main/kotlin/com/kt/learn/domain/Entities.kt
@@ -2,10 +2,20 @@ package com.kt.learn.domain
 
 import java.time.LocalDate
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
 data class Driver(
         @Id
+        @GeneratedValue
         private var id: Long? = null, val name: String, val birthDate: LocalDate
+)
+
+@Entity
+data class Passenger(
+        @Id
+        @GeneratedValue
+        var id: Long? = null,
+        val name: String
 )
--- src/main/kotlin/com/kt/learn/domain/Repositories.kt
+++ src/main/kotlin/com/kt/learn/domain/Repositories.kt
@@ -3,3 +3,5 @@ package com.kt.learn.domain
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface DriverRepository : JpaRepository<Driver, Long>
+
+interface PassengerRepository: JpaRepository<Passenger, Long>
